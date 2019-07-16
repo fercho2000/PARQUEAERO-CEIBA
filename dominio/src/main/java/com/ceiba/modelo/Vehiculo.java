822aef0d31c03abd746c899541542d706dcc415e
José Fernando Usuga Flórez - Ceiba Software <jose.usuga@ceiba.com.co>
2019-07-16 16:26:19 -0500

    Nuevos tests en modelo vehiculo

--- dominio/src/main/java/com/ceiba/modelo/Vehiculo.java
+++ dominio/src/main/java/com/ceiba/modelo/Vehiculo.java
@@ -3,28 +3,21 @@ package com.ceiba.modelo;
 public class Vehiculo {
 
 	private static final int LONGITUD_PLACAS = 6;
-	private static final String MENSAJE_SI_PLACAS_INCORRECTAS="La placa que ingreso es incorrecta";
+	private static final String MENSAJE_SI_PLACAS_INCORRECTAS = "La placa que ingreso es incorrecta";
 	private static final String TODOS_LOS_DATOS_OBLIGATORIOS = "Todos los datos son obligatorio.";
-	
-	
-
 
 	private String placa;
 	private String tipoVehiculo;
 	private String cilindraje;
 	private String marca;
 	private String modelo;
-	
-	
-	public Vehiculo() {
-		
-	}
 
 	public Vehiculo(String placa, String tipoVehiculo, String cilindraje, String marca, String modelo) {
-	ValidarArgumentosVehiculo.validarObligatorios(placa, tipoVehiculo, cilindraje, marca, modelo, TODOS_LOS_DATOS_OBLIGATORIOS);	
-	
-	ValidarArgumentosVehiculo.validarLongitudPlaca(placa, LONGITUD_PLACAS, MENSAJE_SI_PLACAS_INCORRECTAS);
-		
+		ValidarArgumentosVehiculo.validarObligatorios(placa, tipoVehiculo, cilindraje, marca, modelo,
+				TODOS_LOS_DATOS_OBLIGATORIOS);
+
+		ValidarArgumentosVehiculo.validarLongitudPlaca(placa, LONGITUD_PLACAS, MENSAJE_SI_PLACAS_INCORRECTAS);
+
 		this.placa = placa;
 		this.tipoVehiculo = tipoVehiculo;
 		this.cilindraje = cilindraje;
--- dominio/src/test/java/com/ceiba/modelo/VehiculoTest.java
+++ dominio/src/test/java/com/ceiba/modelo/VehiculoTest.java
@@ -1,5 +1,46 @@
 package com.ceiba.modelo;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.ceiba.testdatabuilder.VehiculoTestBuilder;
+
 public class VehiculoTest {
+	private static final String PLACA = "afr32bt";
+	private static final String TODOS_LOS_DATOS_OBLIGATORIOS = "Todos los datos son obligatorio.";
+	private static final String MENSAJE_SI_PLACAS_INCORRECTAS = "La placa que ingreso es incorrecta";
+	private static final String CILINDRAJE = "200";
+	private static final String TIPO_MOTO = "MOTO";
+	private static final String MARCA = "BOXER";
+	private static final String MODELO = "2011";
+
+	@Test
+	public void validarLongitudPlacaEnModelo() {
+		// Arrange
+		VehiculoTestBuilder vehiculoTestBuilder = new VehiculoTestBuilder().conPlaca(PLACA).conCilindraje(CILINDRAJE)
+				.conMarca(MARCA).conModelo(MODELO).conTipoVehiculo(TIPO_MOTO);
+		// act
+		try {
+			vehiculoTestBuilder.build();
+		} catch (Exception e) {
+			// assert
+			assertEquals(MENSAJE_SI_PLACAS_INCORRECTAS, e.getMessage());
+		}
+
+	}
+
+	@Test
+	public void validarArgumentosEnModelo() {
+		// Arrange
+		VehiculoTestBuilder vehiculoTestBuilder = new VehiculoTestBuilder().conPlaca(null).conCilindraje(null)
+				.conMarca(null).conModelo(null).conTipoVehiculo(null);
+		// act
+		try {
+			vehiculoTestBuilder.build();
+		} catch (Exception e) {
+			// assert
+			assertEquals(TODOS_LOS_DATOS_OBLIGATORIOS, e.getMessage());
+		}
+
+	}
 
 }
