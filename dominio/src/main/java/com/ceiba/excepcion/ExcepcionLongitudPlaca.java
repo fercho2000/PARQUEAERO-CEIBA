bc0303e10a5033dc2f660135db99f363dc09ecc8
José Fernando Usuga Flórez - Ceiba Software <jose.usuga@ceiba.com.co>
2019-07-17 17:12:54 -0500

    Correccion

--- dominio/src/main/java/com/ceiba/excepcion/ExcepcionLongitudPlaca.java
+++ dominio/src/main/java/com/ceiba/excepcion/ExcepcionLongitudPlaca.java
@@ -1,11 +1,11 @@
 package com.ceiba.excepcion;
 
-public class ExcepcionLongitudPlaca extends RuntimeException{
+public class ExcepcionLongitudPlaca extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
 	public ExcepcionLongitudPlaca(String message) {
-        super(message);
-    }
-	
+		super(message);
+	}
+
 }
--- dominio/src/test/java/com/ceiba/modelo/VehiculoTest.java
+++ dominio/src/test/java/com/ceiba/modelo/VehiculoTest.java
@@ -6,13 +6,32 @@ import com.ceiba.testdatabuilder.VehiculoTestBuilder;
 
 public class VehiculoTest {
 	private static final String PLACA = "afr32bt";
-	private static final String TODOS_LOS_DATOS_OBLIGATORIOS = "Todos los datos son obligatorio.";
-	private static final String MENSAJE_SI_PLACAS_INCORRECTAS = "La placa que ingreso es incorrecta";
+	private static final String MENSAJE_SI_PLACAS_INCORRECTAS = "La longitud de la placa que ingreso es incorrecta";
+	private static final String MENSAJE_PLACA_OBLIGATORIO = "La placa es obligatoria.";
+	private static final String MENSAJE_TIPO_VEHICULO_OBLIGATORIO = "El tipo de vehiculo es obligatoria.";
+	private static final String MENSAJE_CILINDRAJE_OBLIGATORIO = "El cilindraje es obligatorio.";
+	private static final String MENSAJE_MARCA_OBLIGATORIO = "La marca es obligatorio.";
+	private static final String MENSAJE_MODELO_OBLIGATORIO = "El modelo del vehiculo es obligatorio.";
 	private static final String CILINDRAJE = "200";
 	private static final String TIPO_MOTO = "MOTO";
 	private static final String MARCA = "BOXER";
 	private static final String MODELO = "2011";
 
+	
+	@Test
+	public void validarArgumentoPlacaVaciaEnModeloVehiculo() {
+		// Arrange
+		VehiculoTestBuilder vehiculoTestBuilder = new VehiculoTestBuilder().conPlaca(null);
+		// act
+		try {
+			vehiculoTestBuilder.build();
+		} catch (Exception e) {
+			// assert
+			assertEquals(MENSAJE_PLACA_OBLIGATORIO, e.getMessage());
+		}
+
+	}
+	
 	@Test
 	public void validarLongitudPlacaEnModeloVehiculo() {
 		// Arrange
@@ -29,18 +48,58 @@ public class VehiculoTest {
 	}
 
 	@Test
-	public void validarArgumentosVaciosEnModeloVehiculo() {
+	public void validarArgumentoTipoVehiculoVaciaEnModeloVehiculo() {
 		// Arrange
-		VehiculoTestBuilder vehiculoTestBuilder = new VehiculoTestBuilder().conPlaca(null).conCilindraje(null)
-				.conMarca(null).conModelo(null).conTipoVehiculo(null);
+		VehiculoTestBuilder vehiculoTestBuilder = new VehiculoTestBuilder().conTipoVehiculo(null);
 		// act
 		try {
 			vehiculoTestBuilder.build();
 		} catch (Exception e) {
 			// assert
-			assertEquals(TODOS_LOS_DATOS_OBLIGATORIOS, e.getMessage());
+			assertEquals(MENSAJE_TIPO_VEHICULO_OBLIGATORIO, e.getMessage());
 		}
 
 	}
 
+	@Test
+	public void validarArgumentoCilindrajeVacioEnModeloVehiculo() {
+		// Arrange
+		VehiculoTestBuilder vehiculoTestBuilder = new VehiculoTestBuilder().conCilindraje(null);
+		// act
+		try {
+			vehiculoTestBuilder.build();
+		} catch (Exception e) {
+			// assert
+			assertEquals(MENSAJE_CILINDRAJE_OBLIGATORIO, e.getMessage());
+		}
+
+	}
+	
+	@Test
+	public void validarArgumentoMarcacioEnModeloVehiculo() {
+		// Arrange
+		VehiculoTestBuilder vehiculoTestBuilder = new VehiculoTestBuilder().conMarca(null);
+		// act
+		try {
+			vehiculoTestBuilder.build();
+		} catch (Exception e) {
+			// assert
+			assertEquals(MENSAJE_MARCA_OBLIGATORIO, e.getMessage());
+		}
+	}
+	
+
+	@Test
+	public void validarArgumentoModeloEnModeloVehiculo() {
+		// Arrange
+		VehiculoTestBuilder vehiculoTestBuilder = new VehiculoTestBuilder().conModelo(null);
+		// act
+		try {
+			vehiculoTestBuilder.build();
+		} catch (Exception e) {
+			// assert
+			assertEquals(MENSAJE_MODELO_OBLIGATORIO, e.getMessage());
+		}
+	}
+	
 }
